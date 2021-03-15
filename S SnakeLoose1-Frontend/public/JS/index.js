73b35c7e88cdc56e4d9142b16399e4fec31054cf
s1l3ntq <qimiller89@gmail.com>
2021-03-15 13:10:12 -0500

    Canvas color and size

--- S SnakeLoose1-Frontend/public/JS/index.js
+++ S SnakeLoose1-Frontend/public/JS/index.js
@@ -2,7 +2,11 @@ btnWindow = document.getElementById('btn-group');
 snakeGame = document.getElementById('snake');
 startgame = document.getElementById('startgame');
 
+
 startgame.addEventListener('click', function () {
-    btnWindow.style.display = "none"
+    btnWindow.style.display = "compact"
     snakeGame.style.display = "block"
 });
+
+var canvas = document.getElementById("snakeboard");
+var ctx = canvas.getContext("2d");
