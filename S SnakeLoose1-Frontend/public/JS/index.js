9dad6a5c21122d35ece5ce4ff65d23ee57757027
s1l3ntq <qimiller89@gmail.com>
2021-03-15 15:58:40 -0500

    Keypress logged and working

--- S SnakeLoose1-Frontend/public/JS/index.js
+++ S SnakeLoose1-Frontend/public/JS/index.js
@@ -1,28 +1,56 @@
 btnWindow = document.getElementById('btn-group');
-snakeGame = document.getElementById('snake');
+gameBoard = document.getElementById('gameboard');
 startgame = document.getElementById('startgame');
 
 
 startgame.addEventListener('click', function () {
-    btnWindow.style.display = "compact"
-    snakeGame.style.display = "block"
+    btnWindow.style.display = "none"
+    gameBoard.style.display = "block"
 });
 
 var canvas = document.getElementById("snakeboard");
 var ctx = canvas.getContext("2d");
 
-let snake = [{x:200, y:100},
+let snakeBody = [{x:200, y:100},
     {x:190, y:100},
     {x:180, y:100},
     {x:170, y:100}  
     ]
 
-function createsnake(body) {
-    ctx.fillstyle= "blue";
-    ctx.fillrect(body.x, body.y , 15, 20)
+
+function createSnake(body) {
+    ctx.fillStyle= "blue";
+    ctx.fillRect(body.x, body.y , 15, 20)
 }
 
-function deploysnake() {
-    snake.forEach(createsnake)
+function deploySnake() {
+    snakeBody.forEach(createSnake)
 }
-deploysnake()
+
+deploySnake()
+
+
+
+window.addEventListener("keydown", function(e) {
+    
+    if (e.key === "ArrowUp") {
+        console.log("you pressed up")
+    }
+    else if (e.key === "ArrowDown") {
+        console.log("you pressed down")
+    }
+    else if (e.key === "ArrowRight") {
+        console.log("you pressed right")
+    }
+    else if (e.key === "ArrowLeft") {
+        console.log("you pressed left")
+    }
+    console.log(e)
+  });
+
+function snakeController() {
+const head = {x: snakeBody[0].x, y}
+
+}
+
+//"ArrowUp"	0x26 (38) "ArrowRight"	0x27 (39) "ArrowLeft"	0x25 (37) "ArrowDown"	0x28 (40)
