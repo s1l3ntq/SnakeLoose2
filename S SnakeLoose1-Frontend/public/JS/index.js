128edfda5dbd0349ef845a4048ad57eec42819b4
s1l3ntq <qimiller89@gmail.com>
2021-03-22 22:51:16 -0500

    Refactor code added comments

--- S SnakeLoose1-Frontend/public/JS/index.js
+++ S SnakeLoose1-Frontend/public/JS/index.js
@@ -2,12 +2,13 @@ btnWindow = document.getElementById('btn-group');
 gameBoard = document.getElementById('gameboard');
 startgame = document.getElementById('startgame');
 
-
+//new game button "creating onetime -click toggle" What the function does
 startgame.addEventListener('click', function () {
     btnWindow.style.display = "none"
     gameBoard.style.display = "block"
 });
 
+// variables 
 var canvas = document.getElementById("snakeboard");
 var ctx = canvas.getContext("2d");
 
@@ -15,6 +16,7 @@ var ctx = canvas.getContext("2d");
 let score = 0
 let bodyPart = 0
 
+//snake body coordinate canvas on grid
 let snakeBody = [{x:200, y:100},
     {x:190, y:100},
     {x:180, y:100},
@@ -27,42 +29,22 @@ let xDirection = 10;
 let yDirection = 0;
 
 let food = {
-    x : Math.floor(Math.random()*17+1)* xDirection,
-    y : Math.floor(Math.random()*15+3)* yDirection
+    x : randomTen(0, canvas.width - 10),
+    y : randomTen(0, canvas.height - 10)
 }
 
+let hasEatenIt = snakeBody[0].x === food.x && snakeBody[0].y === food.y
 
-
-function createSnake(body) {
-    ctx.fillStyle= "blue";
-    ctx.fillRect(body.x, body.y , 10, 10);
-    ctx.strokeStyle= "darkblue";  
-    ctx.strokeRect(body.x, body.y, 10, 10);
-}
-
-
-
+//helper function to ramdomize by ten pixels food once snake has eaten it
 function randomTen(min, max) {
     return Math.round((Math.random() * (max-min) + min) / 10) * 10;
 }
 
-function createFood() {
-    foodX = randomTen(0, canvas.width - 10);
-    foodY = randomTen(0, canvas.height - 10);
-    snakeBody.forEach(function isFoodOnSnake(part) {
-        const hasEaten = part.x == foodX && part.y == foodY
-        if (hasEaten)
-        createFood();
-        drawFood()
-    });
+function randomizeFood() {
+    
 }
 
-// function drawFood() {
-//    ctx.beginPath();
-//    ctx.arc(230, 300, 5, 0, Math.PI * 2, false);
-//    ctx.strokeStyle = "white";
-//    ctx.stroke();
-// }
+//creates the instance of the food object 
   function drawFood() {
     ctx.fillStyle = 'red';
     ctx.strokestyle = 'darkred';
@@ -70,50 +52,68 @@ function createFood() {
     ctx.strokeRect(150, 230, 10, 10);
    }
 
-function deploySnake() {
-    snakeBody.forEach(createSnake)
+//creating a single instace of the snake(object)
+function createSnake(body) {
+    ctx.fillStyle= "blue";
+    ctx.fillRect(body.x, body.y , 10, 10);
+    ctx.strokeStyle= "darkblue";  
+    ctx.strokeRect(body.x, body.y, 10, 10);
 }
 
+//creating each instance of the snake(object)
+//polymorphism all inheriting from the same 
+function deploySnake() {
+        snakeBody.forEach(createSnake)
+    }
+    
 
-
+//handles keystrokes 
   window.addEventListener("keydown", function(e) {
     
     if (e.key === "ArrowUp" && yDirection !== 10 ) {
         xDirection = 0;
         yDirection = -10;
-        console.log("you pressed up")
+        
     }
     else if (e.key === "ArrowDown" && yDirection !== -10) {
         xDirection = 0;
         yDirection = 10;
-        console.log("you pressed down")
+        
     }
     else if (e.key === "ArrowRight" && xDirection !== -10) {
         xDirection = 10;
         yDirection = 0;
-        console.log("you pressed right")
+        
     }
     else if (e.key === "ArrowLeft" && xDirection !== 10) {
         xDirection = -10;
         yDirection = 0;  
     }
-    console.log(e)
+
   });
 
 
 
-
-function snakeHead() {
-    var head = {x: snakeBody[0].x+xDirection, y: snakeBody[0].y+yDirection}
+//Defines the direction of the head movement
+function moveSnake() {
+    //Define the direction the head is moving
+    var head = {x: snakeBody[0].x + xDirection, y: snakeBody[0].y + yDirection}
+    //Changing coordinates, adding new positon to the array as snake object moves 
     snakeBody.unshift(head);
-    const hasEatenIt = snakeBody[0].x === food.X && snakeBody[0].y === food.y;
     if (hasEatenIt) {
+        hasEaten()
+
+    } else {
+    //removes the current position of the tail
+     snakeBody.pop();
+    }
+}
+
+function hasEaten(){
+
         score += 10;
         document.getElementById("score").innerHTML = score;
-        createFood();
-    } else {
-    snakeBody.pop();
-    }
+        randomizeFood();
 }
 
 
@@ -123,11 +123,11 @@ function game() {
         if (has_game_ended()) return;
 
         clear()
-        createFood()
+        randomizeFood()
         drawFood()
         //food()
-        snakeHead()
         deploySnake()
+        moveSnake()
         game()
         //snakeEats()
     }, 150)
